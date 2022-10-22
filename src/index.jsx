0af7861ef346f9a0a969a4085df2049e4c3bf0ed
Josh Endemann <jahend44@gmail.com>
2022-10-22 07:19:49 -0500

    fixed errors in index.jsx and updated index.html

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
-// Import statement to indicate that you need to bundle './index.scss
+// Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (will eventually use all the others)
-class myMovieApplication extends React.Component {
+class MyMovieApplication extends React.Component {
   render() {
     return (
       <div className="my-movie">
